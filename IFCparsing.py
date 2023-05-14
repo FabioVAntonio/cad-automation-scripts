4c282d281467736d7979e4c0684cfbc7e226c5bd
Fabio Vicente Antonio <fabio.antonio@outlook.de>
2023-05-14 23:59:28 +0200

    testing the change of objects through ifcopenshell

--- IFCparsing.py
+++ IFCparsing.py
@@ -5,6 +5,7 @@ from ifcopenshell.util.selector import Selector
 
 
 ifc = ifcopenshell.open('./IFCs and PLNs/Test.ifc')
+ifc_objects = ifcopenshell.open('./IFCs and PLNs/Objects.ifc')
 selector = Selector()
 
 
@@ -14,13 +15,19 @@ zones = ifc.by_type('IfcSpace')
 zone = ifc.by_type('IfcSpace')[0]
 door = ifc.by_type('IfcDoor')[0]
 
+object = ifc_objects.by_type('IfcFurniture')
+test_objects = ifc.by_type('IfcFurniture')
+
 #print(wall.get_info())      #gets: id , type, GlobalId, OwnerHistory, Name, etc.. in key/value pairs
 #print(wall.get_info().get('Name'))  #pick out specific info  or in short: print(wall.Name)
 
-zones[2].Name = 'Test room number'
-#print(door.get_info().get('Name'))
-print(zones[2].Name)
-ifc = ifc.write('./IFCs and PLNs/Test.ifc')
+
+test_objects[-2] = object   #changing of objects
+
+print(test_objects[-2])
+
+
+ifc = ifc.write('./IFCs and PLNs/Test.ifc')    #overwrites current IFC file
 
 
 #elements = selector.parse(ifc, '@@ .IfcSpace & ( .IfcDoor  )')
