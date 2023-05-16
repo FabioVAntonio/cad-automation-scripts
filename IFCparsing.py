f2828141bd254b13bf38ea8f3a5f2bd45b509054
Fabio Vicente Antonio <fabio.antonio@outlook.de>
2023-05-16 05:02:17 +0200

    getting location of specific element and its nearest IfcSpace

--- IFCparsing.py
+++ IFCparsing.py
@@ -1,7 +1,10 @@
 # testing IFCOpenshell
-from fileinput import filename
+import multiprocessing
 import ifcopenshell
+import ifcopenshell.geom
+import ifcopenshell.util
 from ifcopenshell.util.selector import Selector
+from ifcopenshell.util.placement import get_local_placement
 
 
 ifc = ifcopenshell.open('./IFCs and PLNs/Test.ifc')
@@ -13,7 +16,7 @@ selector = Selector()
 wall = ifc.by_type('IfcWall')[0]
 zones = ifc.by_type('IfcSpace')
 zone = ifc.by_type('IfcSpace')[0]
-door = ifc.by_type('IfcDoor')[0]
+doors = ifc.by_type('IfcDoor')
 
 object = ifc_objects.by_type('IfcFurniture')
 test_objects = ifc.by_type('IfcFurniture')
@@ -22,15 +25,38 @@ test_objects = ifc.by_type('IfcFurniture')
 #print(wall.get_info().get('Name'))  #pick out specific info  or in short: print(wall.Name)
 
 
-test_objects[-2] = object   #changing of objects
 
-print(test_objects[-2])
+#placement_matrix = get_local_placement(test_objects[-2].ObjectPlacement)
+#print(placement_matrix)
+
+#test_objects[-2] = object   #changing of objects
 
 
-ifc = ifc.write('./IFCs and PLNs/Test.ifc')    #overwrites current IFC file
+
+#print(test_objects[-2])
+
+
+#ifc = ifc.write('./IFCs and PLNs/Test.ifc')    #overwrites current IFC file
 
 
 #elements = selector.parse(ifc, '@@ .IfcSpace & ( .IfcDoor  )')
 #elements_2 = selector.parse(ifc, '@@ .IfcSpace & ( .IfcDoor  )')[4]
-#print(elements_2)
 
+
+
+#location of door[0] (as an example) with its nearest IfcSpace
+matrix = get_local_placement(doors[0].ObjectPlacement)
+location = tuple(map(float, matrix[0:3,3]))
+#print(location)
+
+
+tree_settings = ifcopenshell.geom.settings()
+tree_settings.set(tree_settings.DISABLE_TRIANGULATION, True)
+tree_settings.set(tree_settings.DISABLE_OPENING_SUBTRACTIONS, True)
+it = ifcopenshell.geom.iterator(tree_settings, ifc, include=("IfcSpace",))
+t = ifcopenshell.geom.tree()
+t.add_iterator(it)
+
+# search tree
+a = t.select(location, extend=0.4)
+print(f'Door: {doors[0].Name} belongs to room {a[0].LongName}')
